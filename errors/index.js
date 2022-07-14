549798bb01165c9f37585a57069ff7e154cfd3f4
Desire <desireirankundawilliams@gmail.com>
2022-07-14 16:03:10 +0200

    created new files

---
+++ errors/index.js
@@ -0,0 +1,11 @@
+const CustomAPIError = require('./custom-error')
+const BadRequestError = require('./bad-request')
+const UnthenticatedError = require('./unauthenticated')
+
+
+module.exports = {
+    CustomAPIError,
+    BadRequestError,
+    UnthenticatedError
+
+}
