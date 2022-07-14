abbefc13432e09c3a62b454b56f5a2e0c82db84d
Desire <desireirankundawilliams@gmail.com>
2022-07-14 17:35:08 +0200

    able to store user info to the DB

--- models/User.js
+++ models/User.js
@@ -11,9 +11,16 @@ const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: [true, 'please provide your email address'], 
-        match: ['[a-z0-9]+@[a-z]+\.[a-z]{2,3}', 'please provide valid email']
-    }
+        // match: ['[a-z0-9]+@[a-z]+\.[a-z]{2,3}', 'please provide valid email'],
+        // unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'please provide password'],
+        minlength: 8,
+        maxlength: 12
+    },
 
 })
 
-module.exports = mongoose.
+module.exports = mongoose.model('User',UserSchema)
