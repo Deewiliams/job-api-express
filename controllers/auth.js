286ed7069e5842234322da31654a4cfa80ced81a
Desire <desireirankundawilliams@gmail.com>
2022-07-17 12:19:19 +0200

    able to login with crendentials

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,31 +1,44 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
-const {BadRequestError } = require('../errors')
-const bcrypt = require('bcryptjs') 
+const { BadRequestError } = require('../errors')
+const { UnthenticatedError } = require('../errors')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken');
 
 
 
-const register  = async (req, res) => { 
-    const {name,email,password} = req.body
+const register = async (req, res) => {
+    const { name, email, password } = req.body
 
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password,salt)
+    const hashedPassword = await bcrypt.hash(password, salt)
 
-    const tempUser = {name,email,password:hashedPassword }
-    
-    
-    const user = await User.create({...tempUser})
+    const tempUser = { name, email, password: hashedPassword }
+
+
+    const user = await User.create({ ...tempUser })
     const token = user.createJWT()
-    res.status(StatusCodes.CREATED).json({user: {name: user.name},token})
+    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
 
-const login  = async (req, res) => {
-    res.send('login user')
+const login = async (req, res) => {
+    const { email, password } = req.body
+    if (!email || !password) {
+        throw new BadRequestError('please provide email and password')
+    }
+
+    const user = await User.findOne({ email })
+    //comparing password
+    if (!user) {
+        throw new UnthenticatedError('Invalid Crendentials')
+    }
+    const token = user.createJWT();
+
+    res.status(StatusCodes.OK).json({user: {name: user.name},token})
 }
 
 
-module.exports = { 
+module.exports = {
     register,
     login
 }
